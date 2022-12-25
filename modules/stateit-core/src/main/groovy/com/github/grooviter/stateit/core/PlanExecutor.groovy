ac5129e3f32aa71baec22cd1ee467bb0d775eabe
Mario Garcia <mario.ggar@gmail.com>
2022-12-25 12:15:53 +0100

    feat: adding logback

--- modules/stateit-core/src/main/groovy/com/github/grooviter/stateit/core/PlanExecutor.groovy
+++ modules/stateit-core/src/main/groovy/com/github/grooviter/stateit/core/PlanExecutor.groovy
@@ -2,7 +2,9 @@ package com.github.grooviter.stateit.core
 
 import groovy.json.JsonOutput
 import groovy.transform.TupleConstructor
+import groovy.util.logging.Slf4j
 
+@Slf4j
 @TupleConstructor
 class PlanExecutor {
     Plan plan
@@ -17,6 +19,7 @@ class PlanExecutor {
     private static Result<Plan> toApply(Plan stage) {
         List<Resource> applied = []
         for (Resource resource : stage.resourcesDeclared) {
+            log.info "create ${resource.id}"
             Result<Resource> result = resource.create()
             if (result.isFailure()) {
                 return Result.error(resolvePlan(stage, applied, []), result.error)
@@ -29,12 +32,13 @@ class PlanExecutor {
 
     private static Result<Plan> toRemove(Plan stage) {
         List<Resource> removed = []
-        for (Resource resourceToDestroy : stage.resourcesToRemove) {
-            Result<Resource> result = resourceToDestroy.destroy()
+        for (Resource resource : stage.resourcesToRemove) {
+            log.info "destroy ${resource.id}"
+            Result<Resource> result = resource.destroy()
             if (result.isFailure()) {
                 return Result.error(resolvePlan(stage, [], removed), result.error)
             } else {
-                removed.add(resourceToDestroy)
+                removed.add(resource)
             }
         }
         return Result.of(resolvePlan(stage, [], removed))
@@ -45,6 +49,7 @@ class PlanExecutor {
             return Result.of(stage)
         }
 
+        log.info "save state"
         File stateFile = new File(stage.statePath)
         stateFile.text = JsonOutput.toJson(stage)
         return Result.of(stage)
